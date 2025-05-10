47c19562a10ec7719163266c3a73fa2ab44b57cf
Erebus03 <rajianwar421@gmail.com>
2025-05-10 10:44:14 +0100

    hello

--- main.c
+++ main.c
@@ -89,26 +89,26 @@ int main(int ac, char **av, char **envp)
 	}
 	list_env_vars(&context.envlst, envp);
 
-	t_env_var *tmp = context.envlst;
-	while (tmp) {
-		printf("(%p) [%s=%s]\n", tmp, tmp->key, tmp->value);
-		tmp = tmp->next;
-	}
+	// t_env_var *tmp = context.envlst;
+	// while (tmp) {
+	// 	printf("(%p) [%s=%s]\n", tmp, tmp->key, tmp->value);
+	// 	tmp = tmp->next;
+	// }
 
 	// function to make a 2D array of envp list, store in context.envarr
 	copy_envp(&context);
 	for (int i = 0; context.envarr[i]; i++) {
 		printf("var(%d) = %s\n", i, context.envarr[i]);
 	}
-	write(1, "\nizfinish\n", 10);
+	write(1, "\nizfinish\n\n", 10);
 	
 	while(37)
 	{
   		context.input = readline("\001\033[32m\002minihell $> \001\033[0m\002");
 		if (ft_strncmp(context.input, "exit", 4) == 0)
 				exit(0);
-		//if (validate_quotes(&context) == 0) // this "'"fffFFF"'" cleans to 'fffFFF'
-		//	printf("SYNTAX ERROR: unclosed quotes\n");
+		if (validate_quotes(&context) == 0) // this "'"fffFFF"'" cleans to 'fffFFF'
+			printf("SYNTAX ERROR: unclosed quotes\n");
   	  	printf("u entered '%s'\n", context.input);
 		add_history(context.input);
 		// parse_cmd(lst, cmd);
