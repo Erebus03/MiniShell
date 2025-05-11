abf4746c31558694e7d0e170c747c05d09d6efe0
Erebus03 <rajianwar421@gmail.com>
2025-05-11 11:21:15 +0100

    clean uoyr code

--- utils/add_nodes.c
+++ utils/add_nodes.c
@@ -1,16 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   add_nodes.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: araji <rajianwar421@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/11 10:58:04 by araji             #+#    #+#             */
+/*   Updated: 2025/05/11 10:58:04 by araji            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../minishell.h"
 
 /**
  * Adds a token to the end of the token list
  */
-void add_token(t_token **tokens, t_token *new_token)
+void	add_token(t_token **tokens, t_token *new_token)
 {
-    t_token *current;
-    
+	t_token	*current;
+
     if (!*tokens)
     {
         *tokens = new_token;
-        return;
+        return ;
     }
     current = *tokens;
     while (current->next)
@@ -18,18 +30,17 @@ void add_token(t_token **tokens, t_token *new_token)
     current->next = new_token;
 }
 
-
 /**
  * Adds a command to the end of the command list
  */
-void add_command(t_command **commands, t_command *new_cmd)
+void	add_command(t_command **commands, t_command *new_cmd)
 {
-    t_command *current;
-    
+    t_command	*current;
+
     if (!*commands)
     {
         *commands = new_cmd;
-        return;
+        return ;
     }
     current = *commands;
     while (current->next)
@@ -37,22 +48,20 @@ void add_command(t_command **commands, t_command *new_cmd)
     current->next = new_cmd;
 }
 
-
 /**
  * Adds a redirection to the end of the redirection list
  */
-void add_redir(t_redir **redirs, t_redir *new_redir)
+void	add_redir(t_redir **redirs, t_redir *new_redir)
 {
-    t_redir *current;
-    
+    t_redir	*current;
+
     if (!*redirs)
     {
         *redirs = new_redir;
-        return;
+        return ;
     }
     current = *redirs;
     while (current->next)
         current = current->next;
     current->next = new_redir;
 }
-
